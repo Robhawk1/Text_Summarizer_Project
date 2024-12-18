8061cae910c493431888c6173ad853a55c712344
robhawk <robhawk97@gmail.com>
2024-12-18 15:50:37 +0100

    Updated rest of pipelines

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from textSummarizer.pipeline.stage_01_data_ingenstion import DataIngestionTrainingPipeline
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from textSummarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from textSummarizer.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 from textSummarizer.logging import logger
 
 STAGE_NAME1 = "Data Ingestion stage"
@@ -24,3 +27,38 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME3 = "Data Transformation State"
+
+try:
+    logger.info(f">>> stage {STAGE_NAME3} started <<<")
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
+    logger.info(f">>> stage {STAGE_NAME3} completed <<<\n\n====")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME4 = "Model Trainer stage"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME4} started <<<<<<")
+   model_trainer = ModelTrainerTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME4} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME5 = "Model Evaluation stage"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME5} started <<<<<<")
+   model_evaluation = ModelEvaluationTrainingPipeline()
+   model_evaluation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME5} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
